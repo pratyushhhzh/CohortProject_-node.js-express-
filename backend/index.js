6fc38c08dd12a7a0a3ed138c63d37619e385c88f
Pratyush Sahoo <pratyush.sahoo@hotmail.com>
2026-08-28 13:12:01 +0530

    update PATCH endpoint

--- backend/index.js
+++ backend/index.js
@@ -41,9 +41,78 @@ app.get("/users", async (req, res) => {
       "SELECT * FROM users ORDER BY id ASC"
     );
 
-    res.status(200).json(result.rows);
+    res.status(200).json(result.rows)
+  } catch (error) {
+    console.error(error)
+
+    res.status(500).json({
+      message: "Something went wrong"
+    });
+  }
+});
+
+
+// Post Login Endpoint
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1 AND password = $2",
+      [email, password]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      user: result.rows[0]
+    });
+
   } catch (error) {
     console.error(error);
+
+    res.status(500).json({
+      message: "Something went wrong"
+    });
+  }
+});
+
+//Update (in this we have to enter every field to update , not just any one... its not custom version yet)
+app.patch("/profile/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const { name, registration_no, email, password, age } = req.body
+
+    const result = await pool.query(
+      `UPDATE users
+       SET name = $1,
+           registration_no = $2,
+           email = $3,
+           password = $4,
+           age = $5
+       WHERE id = $6
+       RETURNING *`,
+      [name, registration_no, email, password, age, id]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    res.status(200).json({
+      message: "Profile updated successfully",
+      user: result.rows[0]
+    });
+
+  } catch (error) {
+    console.error(error)
 
     res.status(500).json({
       message: "Something went wrong"
